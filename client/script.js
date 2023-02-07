70053d457d5948240ab8e9992a8782a125fb25da
Infinityvoidd <Kumar.abhinav227@gmail.com>
2023-02-07 16:09:44 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -85,9 +85,9 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
-    // https://infinityvoid.onrender.com
+    // 
 
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://infinityvoid.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
